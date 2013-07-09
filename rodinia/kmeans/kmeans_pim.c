6e1f0da34e7d0011d3f8e3561e72944850c39058
xulifan <xulifan@udel.edu>
2013-07-09 15:54:18 -0700

    add keams nn

---
+++ rodinia/kmeans/kmeans_pim.c
@@ -0,0 +1,123 @@
+#define BLOCK_SIZE_1D 256
+
+void pim_launch_swap_kernel(void *input, void *input_swap, int npoints, int nfeatures, pim_device_id target, cl_event *complete)
+{
+    char * source_nm = (char *)"kmeans.cl";
+    char * kernel_nm = (char *)"kmeans_swap";
+    pim_f gpu_kernel;
+    
+    void * args[1024];
+    size_t sizes[1024];
+    size_t nargs = 0;
+    int dim = 1;
+    int num_pre_event = 0;
+    int num=npoints;
+    size_t globalItemSize,localItemSize;
+
+    localItemSize=BLOCK_SIZE_1D;
+	
+    globalItemSize=(num%localItemSize==0)?num:localItemSize*((num/localItemSize)+1);
+
+    gpu_kernel.func_name = (void*)kernel_nm;
+
+    pim_spawn_args(args,sizes, &nargs, source_nm, (char *)" -D __GPU__", dim, &globalItemSize, &localItemSize, &num_pre_event, NULL, NULL);
+
+    size_t tnargs = nargs;
+
+    // kernel arguments
+
+    args[tnargs] = input;
+    sizes[tnargs] = sizeof(void *);
+    tnargs++;
+
+    args[tnargs] = input_swap;
+    sizes[tnargs] = sizeof(void *);
+    tnargs++;
+
+    args[tnargs] = &npoints;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+    args[tnargs] = &nfeatures;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+
+// insert compeletion event
+
+    args[OPENCL_ARG_POSTEVENT] = complete;
+    sizes[OPENCL_ARG_POSTEVENT] = sizeof(cl_event);
+
+    pim_spawn(gpu_kernel, (void**)args, sizes, tnargs, target, PIM_PLATFORM_OPENCL_GPU);
+
+}
+
+
+void pim_launch_kmeans_kernel(void *feature_swap, void *cluster, void *membership, int npoints, int nclusters, int nfeatures, int offset, int size, pim_device_id target, cl_event *complete)
+{
+    char * source_nm = (char *)"kmeans.cl";
+    char * kernel_nm = (char *)"kmeans_kernel_c";
+    pim_f gpu_kernel;
+    
+    void * args[1024];
+    size_t sizes[1024];
+    size_t nargs = 0;
+    int dim = 1;
+    int num_pre_event = 0;
+    int num=npoints;
+    size_t globalItemSize,localItemSize;
+
+    localItemSize=BLOCK_SIZE_1D;
+	
+    globalItemSize=(num%localItemSize==0)?num:localItemSize*((num/localItemSize)+1);
+
+    gpu_kernel.func_name = (void*)kernel_nm;
+
+    pim_spawn_args(args,sizes, &nargs, source_nm, (char *)" -D __GPU__", dim, &globalItemSize, &localItemSize, &num_pre_event, NULL, NULL);
+
+    size_t tnargs = nargs;
+
+    // kernel arguments
+
+    args[tnargs] = feature_swap;
+    sizes[tnargs] = sizeof(void *);
+    tnargs++;
+
+    args[tnargs] = cluster;
+    sizes[tnargs] = sizeof(void *);
+    tnargs++;
+
+    args[tnargs] = membership;
+    sizes[tnargs] = sizeof(void *);
+    tnargs++;
+
+    args[tnargs] = &npoints;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+    args[tnargs] = &nclusters;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+    args[tnargs] = &nfeatures;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+    args[tnargs] = &offset;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+    args[tnargs] = &size;
+    sizes[tnargs] = sizeof(int);
+    tnargs++;
+
+// insert compeletion event
+
+    args[OPENCL_ARG_POSTEVENT] = complete;
+    sizes[OPENCL_ARG_POSTEVENT] = sizeof(cl_event);
+
+    pim_spawn(gpu_kernel, (void**)args, sizes, tnargs, target, PIM_PLATFORM_OPENCL_GPU);
+
+}
+
+
---
+++ rodinia/nn/pim_utility.h
@@ -0,0 +1,16 @@
+#include <CL/cl.h>
+#include <pim.h>
+
+static const char *getOpenCLErrorString(cl_int err);
+static const char *getPIMErrorString(int err);
+
+uint32_t get_number_pim_compute_cores(pim_device_id pim_id, uint32_t core_type);
+unsigned int find_pims();
+
+void map_pims(int num_pims, int *num_gpus, int *num_cpus, int *gpus_per_pim, int *cpus_per_pim, pim_device_id *list_of_pims);
+//void get_pim_cpu_ids(int num_cpus, pim_device_id *list_of_cpus);
+//void get_pim_gpu_ids(int num_gpus, pim_device_id *list_of_gpus);
+
+void pim_property(int num_pims, int *gpus_per_pim, int *cpus_per_pim, pim_device_id *list_of_pims);
+
+void pim_spawn_args(void **args,size_t *sizes, size_t *nargs, char *ocl_source, char *build_flags, int dim, size_t *glbl, size_t *lcl, int *num_pre_event, cl_event * pre_event_list, cl_event *post_event_list);
